3b400f5de541be0f57efcd92b302eb255381ea1e
Brian Henderson <bhenderson@riparian.com>
2020-10-14 07:57:55 -0700

    Add different levels.

--- memorizer/js/scripts.js
+++ memorizer/js/scripts.js
@@ -1,31 +1,66 @@
+const $ = document.querySelector.bind(document)
+const $$ = document.querySelectorAll.bind(document)
+
 function isWord(word) {
   return word && word.length &&  word.match(/[a-z]/i)
 }
 
 function next() {
-  const words = Array.from(document.querySelectorAll('.word'))
-
   return (event) => {
     if (event.type === 'keypress' && event.code !== 'Space') return
     event.preventDefault()
 
-    const word = words.shift()
+    const word = $('.word.blank')
 
-    word.innerText = word.dataset.original
+    if (word) show(word)
   }
 }
 
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1)); // random index from 0 to i
+
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
+function reset() {
+  const words = Array.from($$('.word'))
+  const level = Number( $('#level').value )
+
+  shuffle(words)
+
+  for (let i = 0; i<words.length; i++) {
+    const isHide = i % 5 < level
+    const word = words[i]
+
+    isHide ? hide(word) : show(word)
+  }
+}
+
+function show(word) {
+  word.innerText = word.dataset.original
+  word.classList.remove('blank')
+}
+
+function hide(word) {
+  word.innerText = word.dataset.hidden
+  word.classList.add('blank')
+}
+
 function save() {
-  document.getElementById('form').style.display = "none"
-  const text = document.getElementById('input').value
+  $('#form').style.display = "none"
+  const text = $('#input').value
 
   localStorage.setItem('text', text)
 
   generate(text)
+
+  reset()
 }
 
 function generate(text) {
-  const div = document.getElementById('main')
+  const div = $('#main')
 
   text.split(/\n/).forEach(sentence => {
     sentence.split(/\b/).forEach(word => {
@@ -36,7 +71,7 @@ function generate(text) {
 
         newSpan.dataset.original = word
 
-        word = word.substr(0, 1) +
+        newSpan.dataset.hidden = word.substr(0, 1) +
            word.substr(1).replaceAll(/./g, '_')
       }
 
@@ -50,12 +85,22 @@ function generate(text) {
 
   const listener = next()
   document.addEventListener('keypress', listener)
-  document.addEventListener('mousedown', listener)
+  div.addEventListener('mousedown', listener)
   //document.addEventListener('touchstart', listener)
 }
 
+function levelChange(num) {
+  const el = $('#level')
+  const value = Number(el.value)
+  const newVal = Math.min( Math.max( value + num, 0 ), 5 )
+
+  el.value = newVal
+
+  if (value !== newVal) reset()
+}
+
 function main() {
-  document.getElementById('input').value = localStorage.getItem('text')
+  $('#input').value = localStorage.getItem('text')
 }
 
 main()
